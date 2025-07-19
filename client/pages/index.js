570b3221a1e294fd669464b4b440339b577f7efd
irfan <irfankhan1120010@gmail.com>
2025-07-19 16:58:18 +0530

    removing the error pages/index.js

--- client/pages/index.js
+++ client/pages/index.js
@@ -90,7 +90,7 @@ export default function Home() {
                   Customize
                 </Link>
               </div>
-            </motion.di   v>
+            </motion.div>
           </div>
         </div>
       </section>
